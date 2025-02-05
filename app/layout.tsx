947770581cc5a876d46e38d360a6a105aef196d8
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-02-05 18:17:44 +0100

    refactor: removed useless file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import "./globals.css";
+import type React from "react"
+import { Dock } from "./components/Dock";
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="fr">
       <body>
         {children}
+        <Dock />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import { Dock } from "./components/Dock";
-import { HomePage } from "./components/HomePage";
+import { Particles } from "@/components/ui/particles"
+import Image from "next/image"
 
 export default function Home() {
+  const color = "#ffffff"
+
   return (
-    <div className="h-screen w-full">
-      <Dock />
-      <HomePage />
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
+      <div className="-translate-y-20 z-10 flex flex-col items-center">
+        <Image
+          src="/profilePicture.jpg"
+          width={180}
+          height={180}
+          alt="profilePicture"
+          className="pointer-events-none mb-24 select-none rounded-full border-[4px] border-foreground object-cover object-right-bottom"
+        />
+        <span className="pointer-events-none z-10 whitespace-pre-wrap text-center font-semibold text-8xl leading-none">Quentin Lapierre.</span>
+      </div>
+      <Particles className="absolute inset-0 z-0 h-screen" quantity={200} ease={10} color={color} refresh />
     </div>
   )
 }
