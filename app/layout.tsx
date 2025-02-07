9ba0146dd3eddd51fb26ab66ee5b85e0fc0a283f
Quentin Lapierre <quentin.lapierre@epitech.eu>
2025-02-07 18:01:38 +0100

    feat: added website metadata (title, desc, favicon)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import "./globals.css";
 import type React from "react"
 import { Dock } from "./components/Dock";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Quentin L. portfolio",
+}
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="fr">
       <body>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
         {children}
         <Dock />
       </body>
